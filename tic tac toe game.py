b7f336d67b0c59b346fab71465d0f0e3b817c903
otomdee <otomdaniel65@gmail.com>
2023-10-04 18:57:42 +0100

    wireframe practice linkedin landing page

--- tic tac toe game.py
+++ tic tac toe game.py
@@ -1,17 +1,18 @@
 import time
 
 
-#dict of x values
+#dict of x values(determines what is shown on the board)
 x_dict = {"x1":1, "x2":2, "x3":3, "x4":4, "x5":5, "x6":6, "x7":7, "x8":8, "x9":9}
-#dict of r values
+#dict of r values(determines win)
 r_dict = {"r1":0, "r2":0, "r3":0, "r4":0, "r5":0, "r6":0, "r7":0, "r8":0, "r9":0}
 
 r_dict_list = list(r_dict.values())
 win_or_lose = False
 
+#print game board from x_dict 
 def print_board():
     print("|", x_dict["x1"],"|",x_dict["x2"],"|",x_dict["x3"],"|"," \n|",x_dict["x4"],"|",x_dict["x5"],"|",x_dict["x6"],"|"," \n|",x_dict["x7"],"|",x_dict["x8"],"|",x_dict["x9"],"|")
-
+#reset board 
 def board_reset():
     global x_dict
     global r_dict
@@ -22,7 +23,7 @@ def board_reset():
     r_dict_list = list(r_dict.values())
     win_or_lose = False
 
-#scoring logic
+#scoring on r_dict based on X or O in x_dict(on the board)
 def scoring():    
     for i in x_dict:
         if x_dict[i] == "X":
@@ -37,52 +38,47 @@ def win_logic():
     global win_or_lose
     global r_dict_list
     r_dict_list = list(r_dict.values())
-    #check rows for X
-    
+#check rows for X
     if sum(r_dict_list[:3]) == 30:
         win_or_lose = True
     elif sum(r_dict_list[3:6]) == 30:
         win_or_lose = True
     elif sum(r_dict_list[6:]) == 30:
         win_or_lose = True
-    #check columns for X
+#check columns for X
     if sum(r_dict_list[:7:3]) == 30:
         win_or_lose = True
     elif sum(r_dict_list[1:8:3]) == 30:
         win_or_lose = True
     elif sum(r_dict_list[2::3]) == 30:
         win_or_lose = True
-
-    #check diagonals for X
+#check diagonals for X
     if sum(r_dict_list[::4]) == 30:
         win_or_lose = True
     elif sum(r_dict_list[2:7:2]) == 30:
         win_or_lose = True
-
-    #check rows for O
-    
+#check rows for O  
     if sum(r_dict_list[:3]) == 150:
         win_or_lose = True
     elif sum(r_dict_list[3:6]) == 150:
         win_or_lose = True
     elif sum(r_dict_list[6:]) == 150:
         win_or_lose = True
-    #check columns for O
+#check columns for O
     if sum(r_dict_list[:7:3]) == 150:
         win_or_lose = True
     elif sum(r_dict_list[1:8:3]) == 150:
         win_or_lose = True
     elif sum(r_dict_list[2::3]) == 150:
         win_or_lose = True
-
-    #check diagonals for O
+#check diagonals for O
     if sum(r_dict_list[::4]) == 150:
         win_or_lose = True
     elif sum(r_dict_list[2:7:2]) == 150:
         win_or_lose = True
 
 
-    
+#player 1 plays with X   
 def p1_play():
     global win_or_lose
     global r_dict_list
@@ -90,14 +86,14 @@ def p1_play():
     p1_input_str = input("Pick a number to play(X): ")
     p1_input = int(p1_input_str)
     x_dict["x{}".format(p1_input)] = "X"
-    #print the current board
+#print the current board
     print_board()
     scoring()
     win_logic()
     if win_or_lose == True:
         print("Congrats, X wins!")
         
-        
+#player 2 plays with O        
 def p2_play():        
     global win_or_lose
     global r_dict_list
@@ -105,13 +101,13 @@ def p2_play():
     p2_input_str = input("Pick a number to play(O): ")
     p2_input = int(p2_input_str)
     x_dict["x{}".format(p2_input)] = "O"
-
     print_board()
     scoring()
     win_logic()
     if win_or_lose == True:
         print("Congrats, O wins!")
 
+#loop to allow multiple turns and restart the game after a win
 def game():
     print_board()
     while win_or_lose == False:
