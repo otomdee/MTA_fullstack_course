020f1a19228e5c1bbb407d4b8337d838cf114db6
otomdee <otomdaniel65@gmail.com>
2023-11-01 09:19:04 +0100

    cleaned up folders

---
+++ c/mario.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int height = 4;
+    int i = 0;
+    while(i < 4)
+    {
+        int j = 4;
+        for (n = 0; n < (j-1); n++)
+        {
+            printf("");
+        }
+        for (k = 0; k < i+1; k++)
+        {
+            printf("#");
+        }
+        j--;
+        i++;
+    }
+}
---
+++ python/classwork python 06_09_2023.py
@@ -0,0 +1,10 @@
+"""This program prints the names of players in Manchester City football club
+"""
+
+player_list = ["Haaland", "Alvarez", "Foden", "KDB", "Jack Grealish", "Rodri", "Ake", "Walker", "Akanji", "Ruben Dias", "Ederson"]
+
+for player in player_list:
+    print(player)
+
+if len(player_list) == 11:
+    print("winning team")
